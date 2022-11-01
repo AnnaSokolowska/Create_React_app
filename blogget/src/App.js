f52239dab64af0c6c18aa9cee8ad4001565ee8cf
Anna Sokolowska <milanka.anka@gmail.com>
2022-11-01 20:07:52 +0100

    hygen

--- blogget/src/App.js
+++ blogget/src/App.js
@@ -1,9 +1,11 @@
-
+import Header from './components/Header';
+import Main from './components/Main';
 function App() {
   return (
-      <header className="App-header">
-        <p>Hello React!</p>
-      </header>
+ <>
+  <Header />
+  <Main />
+ </>
   );
 }
 
